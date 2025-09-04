cef6a95d4fb77f9804ef880cf60a4d46d10369ab
lucilacabello <luli@gardenlife.com.ar>
2025-09-04 12:35:34 -0300

    Update health.js

--- api/health.js
+++ api/health.js
@@ -1,3 +1,5 @@
+// /api/health.js
 export default function handler(req, res) {
-  res.status(200).send('OK');
+  res.setHeader('Content-Type', 'text/html; charset=utf-8');
+  res.status(200).send('<h1>Gardenlife MODO</h1><p>OK</p>');
 }
